39edf7757e285cd15e3ee0f62734c1a27420f94e
trung-dev <trungezeal115@gmail.com>
2023-10-24 08:53:54 +0700

    up fe

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const baseURL = "https://backend.betaz.io/";
-const baseURL = "http://localhost:3001/";
+const baseURL = process.env.REACT_APP_BACKEND_API || "http://localhost:3001/";
 
 export const clientAPI = async (method, url, options) => {
   if (!options) options = {};
